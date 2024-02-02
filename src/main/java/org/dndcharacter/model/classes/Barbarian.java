eb5d8b696acbe2ca9f7891d9492f3724ee123f27
Noah Stevens <stevensnas12@gmail.com>
2024-02-02 13:37:06 -0500

    Added saving throws, skills, and proficiencies relevant to Barbarians

--- src/main/java/org/dndcharacter/model/classes/Barbarian.java
+++ src/main/java/org/dndcharacter/model/classes/Barbarian.java
@@ -5,5 +5,18 @@ public class Barbarian extends BaseClass{
     public Barbarian() {
         name = "Barbarian";
         hitDie = 12;
+        savingThrows.add("Strength");
+        savingThrows.add("Constitution");
+        skills.add("Animal Handling");
+        skills.add("Athletics");
+        skills.add("Intimidation");
+        skills.add("Nature");
+        skills.add("Perception");
+        skills.add("Survival");
+        proficiencies.add("Light Armor");
+        proficiencies.add("Medium Armor");
+        proficiencies.add("Shields");
+        proficiencies.add("Simple Weapons");
+        proficiencies.add("Martial Weapons");
     }
 }
