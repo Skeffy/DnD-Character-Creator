d0a1549070f3ea481886f9023ddf9c0745377f34
Noah Stevens <stevensnas12@gmail.com>
2024-02-04 23:59:59 -0500

    added loop for putting saving throw and skill modifier data into characterData map

--- src/main/java/org/dndcharacter/model/CharacterSheet.java
+++ src/main/java/org/dndcharacter/model/CharacterSheet.java
@@ -5,40 +5,40 @@ import java.util.Map;
 
 public class CharacterSheet {
 
-    Map<String, String> textFields = new HashMap<>();
+    Map<String, String> characterData = new HashMap<>();
 
     public CharacterSheet(Character character) {
         //general info
-        textFields.put("CharacterName", character.getName());
-        textFields.put("ClassLevel", character.getClassLevel());
-        textFields.put("Background", character.getBackgroundName());
-        textFields.put("Race", character.getRaceName());
-        textFields.put("Alignment", character.getAlignment());
+        characterData.put("CharacterName", character.getName());
+        characterData.put("ClassLevel", character.getClassLevel());
+        characterData.put("Background", character.getBackgroundName());
+        characterData.put("Race", character.getRaceName());
+        characterData.put("Alignment", character.getAlignment());
 
-        textFields.put("ProfBonus", character.getProfBonus());
+        characterData.put("ProfBonus", "+" + character.getProfBonus().toString());
         //textFields.put("AC", );
-        textFields.put("Initiative", character.getStatModifiers().get("Dexterity").toString());
-        textFields.put("Speed", character.getSpeed());
-        textFields.put("HPMax", character.getHp());
-        textFields.put("HPCurrent", character.getHp());
+        characterData.put("Initiative", character.getStatModifiers().get("Dexterity").toString());
+        characterData.put("Speed", character.getSpeed());
+        characterData.put("HPMax", character.getHp());
+        characterData.put("HPCurrent", character.getHp());
 
         //base stats and modifiers
-        textFields.put("STR", character.getStats().get("Strength").toString());
-        textFields.put("DEX", character.getStats().get("Dexterity").toString());
-        textFields.put("CON", character.getStats().get("Constitution").toString());
-        textFields.put("INT", character.getStats().get("Intelligence").toString());
-        textFields.put("WIS", character.getStats().get("Wisdom").toString());
-        textFields.put("CHA", character.getStats().get("Charisma").toString());
-        textFields.put("STRmod", character.getStatModifiers().get("Strength").toString());
-        textFields.put("DEXmod", character.getStatModifiers().get("Dexterity").toString());
-        textFields.put("CONmod", character.getStatModifiers().get("Constitution").toString());
-        textFields.put("INTmod", character.getStatModifiers().get("Intelligence").toString());
-        textFields.put("WISmod", character.getStatModifiers().get("Wisdom").toString());
-        textFields.put("CHAmod", character.getStatModifiers().get("Charisma").toString());
+        characterData.put("STR", character.getStats().get("Strength").toString());
+        characterData.put("DEX", character.getStats().get("Dexterity").toString());
+        characterData.put("CON", character.getStats().get("Constitution").toString());
+        characterData.put("INT", character.getStats().get("Intelligence").toString());
+        characterData.put("WIS", character.getStats().get("Wisdom").toString());
+        characterData.put("CHA", character.getStats().get("Charisma").toString());
+        characterData.put("STRmod", character.getStatModifiers().get("Strength").toString());
+        characterData.put("DEXmod", character.getStatModifiers().get("Dexterity").toString());
+        characterData.put("CONmod", character.getStatModifiers().get("Constitution").toString());
+        characterData.put("INTmod", character.getStatModifiers().get("Intelligence").toString());
+        characterData.put("WISmod", character.getStatModifiers().get("Wisdom").toString());
+        characterData.put("CHAmod", character.getStatModifiers().get("Charisma").toString());
 
         //saving throws and skills
         for (String field : character.getSkillAndSaveProficiencies()) {
-            textFields.put(field + "CheckBox", "Yes");
+            characterData.put(field + "CheckBox", "Yes");
         }
 
         //features and traits
@@ -46,17 +46,22 @@ public class CharacterSheet {
         for (String feature : character.getFeaturesAndTraits()) {
             featuresAndTraits += feature + " \n\n";
         }
-        textFields.put("Features and Traits", featuresAndTraits);
+        characterData.put("Features and Traits", featuresAndTraits);
 
         //languages and proficiencies
         String proficiencies = "";
         for (String proficiency : character.getProficienciesAndLanguages()) {
             proficiencies += proficiency + " \n";
         }
-        textFields.put("ProficienciesLang", proficiencies);
+        characterData.put("ProficienciesLang", proficiencies);
+
+        //saving throw and skill modifiers
+        for (String skill : character.getSkillAndSaveModifiers().keySet()) {
+            characterData.put(skill, character.getSkillAndSaveModifiers().get(skill).toString());
+        }
     }
 
     public Map<String, String> getTextFields() {
-        return textFields;
+        return characterData;
     }
 }
