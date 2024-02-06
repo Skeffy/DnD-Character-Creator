a95ab1a1438f69e7259c66bff8fe16564e2b0358
Noah Stevens <stevensnas12@gmail.com>
2024-02-05 21:18:09 -0500

    Refactored to only instantiate console and controller then pass controll to the controller

--- src/main/java/org/dndcharacter/Application.java
+++ src/main/java/org/dndcharacter/Application.java
@@ -1,27 +1,10 @@
 package org.dndcharacter;
 
-import org.dndcharacter.model.Character;
-import org.dndcharacter.model.CharacterSheet;
-import org.dndcharacter.model.backgrounds.Acolyte;
-import org.dndcharacter.model.backgrounds.Background;
-import org.dndcharacter.model.classes.Barbarian;
-import org.dndcharacter.model.classes.BaseClass;
-import org.dndcharacter.model.races.Dwarf;
-import org.dndcharacter.model.races.Race;
-
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
-        List<String> statNames = List.of("Strength", "Constitution", "Charisma", "Wisdom", "Intelligence", "Dexterity");
-        Race dwarf = new Dwarf(null);
-        BaseClass barbarian = new Barbarian();
-        Background acolyte = new Acolyte("Elvish", "Draconic");
-        Character character = new Character("Gregulon Stunmkas", barbarian, dwarf, acolyte, "Chaotic Neutral", statNames);
-        CharacterSheet characterSheet = new CharacterSheet(character);
-        PdfManager pdfManager = new PdfManager(character.getName());
-        pdfManager.write(characterSheet.getTextFields());
-        pdfManager.close();
+        Console console = new Console();
+        Controller controller = new Controller(console);
+        controller.run();
     }
 }
